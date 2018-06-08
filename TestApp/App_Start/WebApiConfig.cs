64ec05fc55f896132135f7b289e0fa1568575f2a
Герций Елена <gertsij.elena@gmail.com>
2018-06-08 13:55:03 +0300

    configure web api

---
+++ TestApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using System.Net.Http.Headers;
+
+namespace TestApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Маршруты веб-API
+            config.MapHttpAttributeRoutes();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Routes.MapHttpRoute(
+               name: "ApiByActionAndId",
+               routeTemplate: "api/{controller}/{action}/{id}",
+               defaults: new { action = "Get" }
+            );
+        }
+    }
+}
