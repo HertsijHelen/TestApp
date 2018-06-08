e76bc3074f09b527c3960a27e1a123bcb99e9588
Герций Елена <gertsij.elena@gmail.com>
2018-06-08 12:44:38 +0300

    add Entity

---
+++ TestApp/Entities/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace TestApp.Entities
+{
+    public class DataContext:DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ TestApp/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestApp.Entities
+{
+    public class Employee
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(50, ErrorMessage = "Not more than 50 Symbols")]
+        public string Name { get; set; }
+
+        [Display(Name = "Position")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(512, ErrorMessage = "Not more than 50 Symbols")]
+        public string Position { get; set; }
+
+        [Display(Name = "Age")]
+        [Required(ErrorMessage = "Required")]
+        public int Age { get; set; }
+
+        [Display(Name = "StartDate")]
+        [Required(ErrorMessage = "Required")]
+        public DateTime StartDate { get; set; }
+    }
+}
